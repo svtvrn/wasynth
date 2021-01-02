c39e2f746634ef1274c5ba8f9c925116585504cc
svtvrn <nikoschasapis99@gmail.com>
2021-01-02 19:46:04 +0200

    Added biquad filters

--- app.js
+++ app.js
@@ -1,59 +1,86 @@
-
+//Creates the audio context and the starting wave type
 var context = new (window.AudioContext || window.webkitAudioContext)();
 var wave='sine'
 
+//Master gain node
 var  gain = context.createGain();
 gain.gain.value=0.5;
 gain.connect(context.destination);
 gainval.innerHTML = (gain.gain.value*100).toFixed(0);
 document.getElementById("currwave").innerHTML = "Current wave: "+wave;
 
+
+//Compressor node, applies effect when the button is pressed
 var compressor = context.createDynamicsCompressor();
 compressor.threshold.setValueAtTime(-50, context.currentTime);
 compressor.knee.setValueAtTime(40, context.currentTime);
 compressor.ratio.setValueAtTime(12, context.currentTime);
 compressor.attack.setValueAtTime(0, context.currentTime);
 compressor.release.setValueAtTime(0.25, context.currentTime);
+compressor_on = 0
 
+//Bass, Mid and Treble filters, gain aka boost changed by user.
+var lowshelf = context.createBiquadFilter();
+lowshelf.type = "lowshelf";
+lowshelf.frequency.value = 4000;
+lowshelf.gain.value = 2;
+
+var peaking = context.createBiquadFilter();
+peaking.type = "peaking";
+peaking.frequency.value = 10000;
+peaking.gain.value = -2;
+
+var highself = context.createBiquadFilter();
+highself.type = "highself";
+highself.frequency.value = 17000;
+highself.gain.value = 2;
+
+//ASDR variables used to apply the effect
 var attack = delay = release = 0.1;
 var sustain = 1, envelopeMode = 1, velocity = 1;
 
-
-compressor_on = 0
-
+//Assigns the "keys" elements from the HTML file
 var keys = document.querySelector("#keys");
 keys.addEventListener("mousedown",playNote);
 
+//Assigns the "wave button" elements from the HTML file
 var waves = document.querySelector("#waves");
 waves.addEventListener("click",changeWave);
 
+//Assigns the "compressor button" element from the HTML file
 var compression = document.querySelector("#compression");
 compression.addEventListener("click",compressionOn);
 
+//Creates the note for the respective key, C4 to C5 and applies the filters (bass,mid,treb,compression)
 function createNote(hertz){
     var note = context.createOscillator();
     note.type=wave;
     note.frequency.setValueAtTime(hertz,context.currentTime);
+    note.connect(lowshelf);
+    lowshelf.connect(peaking);
+    peaking.connect(highself);
     if(compressor_on){
-        note.connect(compressor);
+        highself.connect(compressor);
     }else{
-        note.connect(gain);
+        highself.connect(gain);
     }
     return note;
 }
 
+//Playes the note created in createNote(), applies ASDR accordingly
 function playNote(e){
     if(e.target!==e.currentTarget){
         var hertz= parseFloat(e.target.id);
     }
     var note = createNote(hertz);
     note.start();
-    envelopeOn(gain,attack,delay,sustain);
+    //envelopeOn(gain,attack,delay,sustain);
     keys.addEventListener("mouseup",function(){
-        envelopeOff(gain,release);
+        note.stop();
     });
 }
 
+//Changes gain when the slider value is changed
 function changeGain(e){
     document.getElementById('volume').addEventListener('input', function() {
         gain.gain.value=volume.value;
