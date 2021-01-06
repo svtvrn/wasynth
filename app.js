f5b580777626822c16eb72a13f8967a90d06a696
svtvrn <nikoschasapis99@gmail.com>
2021-01-06 15:44:24 +0200

    update osc

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ var wave='sine'
 sine.style.color ='rgb(24, 255, 101)';
 sine.style.backgroundColor = 'rgb(32, 10, 46)';
 
+//Oscilloscope node and variables
+var analyser = context.createAnalyser();
+analyser.fftSize = 2048;
+
 //Master gain node
 var  amp = context.createGain();
 amp.gain.value=0.5;
 amp.connect(context.destination);
+amp.connect(analyser);
 gainval.innerHTML = (amp.gain.value*100).toFixed(0)+"%";
 
 //Distortion filter
@@ -35,10 +40,12 @@ highshelf.gain.value = 0;
 var envelopeGain = context.createGain();
 var attack = decay = release = 0.5;
 var sustain = 0.5, envelopeMode = 1;
+lowenv.style.color='rgb(24, 255, 101)';
+lowenv.style.backgroundColor = '#830044';
 
 var tremolo = context.createOscillator();
 tremolo.frequency.value = 10;
-tremolo.type = 'sawtooth';
+tremolo.type = wave;
 tremolo.connect(amp.gain);
 tremolo.start();
 
@@ -59,6 +66,18 @@ keys.addEventListener("mousedown",playNote);
 var waves = document.querySelector("#waves");
 waves.addEventListener("click",changeWave);
 
+var envModeButtons = document.querySelector("#envmode");
+envModeButtons.addEventListener("click",changeEnvelopeMode);
+
+var oscilloscopeContext = document.getElementById("oscilloscope").getContext("2d");
+oscilloscope();
+
+function oscilloscope(){
+    drawOscilloscope(analyser,oscilloscopeContext);
+    requestAnimationFrame(oscilloscope);
+}
+
+
 //Creates the note for the respective key, C4 to C5 and applies the filters (bass,mid,treb,compression)
 function createNote(hertz){
     var note = context.createOscillator();
@@ -72,7 +91,6 @@ function playNote(e){
     if(e.target!==e.currentTarget){
         var hertz = parseFloat(e.target.id);
     }
-    console.log(hertz);
     var note = createNote(hertz);
     note.connect(filters);
     note.start();
@@ -87,6 +105,13 @@ function changeGain(e){
     document.getElementById('volume').addEventListener('input', function() {
         amp.gain.value=volume.value;
         gainval.innerHTML = (amp.gain.value*100).toFixed(0)+"%";
+        if(amp.gain.value<0.01){
+            tremolo.disconnect();
+        }else{
+            if(tremolo.disconnect()==undefined){
+                tremolo.connect(amp.gain);
+            }
+        }
     });
 }
 
@@ -96,20 +121,19 @@ function changeBass(e){
     });
 }
 
-function changeMid(e){
-    document.getElementById('mid').addEventListener("input",function(){
-        peaking.gain.value = mid.value;
-    });
-}
-
 function changeTreble(e){
     document.getElementById('treble').addEventListener("input",function(){
         highshelf.gain.value = treble.value;
     });
 }
+
 function changeDistortion(e){
     document.getElementById('distort').addEventListener("input",function(){
-        distortion.curve = makeDistortionCurve(distort.value*5);
+        if(distortButton.title=='on'){
+            distortion.curve = makeDistortionCurve(distort.value*5);
+        }else{
+            return;
+        }
     });
 }
 
@@ -134,6 +158,7 @@ function changeRelease(e){
     });
 }
 
+//Changes the oscillator wave type
 function changeWave(e){
     if(e.target!==e.currentTarget){
         wave= e.target.id;
@@ -146,6 +171,19 @@ function changeWave(e){
     }
 }
 
+function changeEnvelopeMode(e){
+    if(e.target!==e.currentTarget){
+        envelopeMode = parseInt(e.target.value);
+        [].slice.call(document.getElementsByClassName('envelopebutton'), 0).forEach(function(element){
+            element.style.color='whitesmoke';
+            element.style.backgroundColor = '#ff006a';
+        });
+        e.target.style.color='rgb(24, 255, 101)';
+        e.target.style.backgroundColor = '#830044';
+    }
+}
+
+//Turns on the envelope effect, from attack till sustain
 function envelopeOn(gain,attack,decay,sustain){
     var time = context.currentTime;
     attack *= envelopeMode;
@@ -156,6 +194,7 @@ function envelopeOn(gain,attack,decay,sustain){
     gain.linearRampToValueAtTime(sustain,time+attack+decay);
 }
 
+//Releases the envelope effect
 function envelopeOff(gain,release,note){
     var time = context.currentTime;
     release *= envelopeMode;
@@ -165,22 +204,24 @@ function envelopeOff(gain,release,note){
     note.stop(time+release);
 }
 
+//Button that turns on or off the distortion effect
 function turnOnDistortion(e){
     if(distortButton.title=='off'){
         distortButton.setAttribute('title','on');
         distortButton.innerHTML="ON";
         distortButton.style.color =  'rgb(24, 255, 101)';
-        distortButton.style.backgroundColor = 'rgb(100, 10, 156)';
+        distortButton.style.backgroundColor = '#830044';
         distortion.curve = makeDistortionCurve(document.getElementById('distort').value*5);
     }else if(distortButton.title=='on'){
         distortButton.setAttribute('title','off');
         distortButton.innerHTML="OFF";
         distortButton.style.color =  'whitesmoke';
-        distortButton.style.backgroundColor = 'rgb(144, 10, 156)';
+        distortButton.style.backgroundColor = '#ff006a';
         distortion.curve = makeDistortionCurve(0);
     }
 }
 
+//Distorts the oscillator's wave curve
 function makeDistortionCurve(amount) {
     var k = typeof amount === 'number' ? amount : 0,
       n_samples = 44100,
@@ -193,4 +234,33 @@ function makeDistortionCurve(amount) {
       curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
     }
     return curve;
+}
+
+function drawOscilloscope(analyser,context){
+
+    var height = context.canvas.height;
+    var width = context.canvas.width;
+    var timeData = new Uint8Array(analyser.frequencyBinCount);
+    var scaling = height / 256;
+    var risingEdge = 0;
+    var edgeThreshold = 5;
+    analyser.getByteTimeDomainData(timeData);
+
+    context.fillStyle = 'rgb(19, 18, 19)';
+    context.fillRect(0, 0, width, height);
+    context.lineWidth = 3;
+    context.strokeStyle = ' rgb(24, 255, 101)';
+    context.beginPath();
+
+    while (timeData[risingEdge++] - 128 > 0 && risingEdge <= width);
+        if (risingEdge >= width) risingEdge = 0;
+
+    while (timeData[risingEdge++] - 128 < edgeThreshold && risingEdge <= width);
+        if (risingEdge >= width) risingEdge = 0;
+
+    for (var x = risingEdge; x < timeData.length && x - risingEdge < width; x++)
+        context.lineTo(x - risingEdge, height - timeData[x] * scaling);
+
+    context.stroke();
+
 }
